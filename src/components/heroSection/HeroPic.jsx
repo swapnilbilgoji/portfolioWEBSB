a57fe69a264edbb220d46e0e212c0650a6a5b503
Swapnil Bilgoji <138004287+swapnilbilgoji@users.noreply.github.com.>
2025-07-05 10:53:17 +0530

    change image name

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/SwapnilSwapnil.png"
+        src="../../public/images/SwapnilHexapic.png"
         alt="Swapnil Bilgoji"
         className="max-h-[450px] w-auto"
       />
